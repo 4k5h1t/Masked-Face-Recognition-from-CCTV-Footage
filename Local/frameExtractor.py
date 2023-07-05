b37fa26de5adb83c1bdd59fe3fcdd7cf8fd2d1f9
4k5h1t <ask160203@gmail.com~>
2023-07-06 02:59:29 +0530

    Update 1 - 5/7/23

--- Local/frameExtractor.py
+++ Local/frameExtractor.py
@@ -1,24 +1,40 @@
-def frameExtractor():
-    if os.path.exists('images'):
-        shutil.rmtree('../G-Drive/images') #Deleting existing directory to remove garbage files
-    cam = cv2.VideoCapture(vid) # video file input
-#   face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  
-    try:
-        if not os.path.exists('images'):#Creating images folder to store video frames
-            os.makedirs('images')
-            print("Created")
-    except OSError:
-        print('Error: Creating directory of images') #Error message
-    currentframe = 0
-    while (True):
-        ret, frame = cam.read()
-        if ret:
-            frameDetector(frame)
-            # name = './images/frame' + str(currentframe) + '.jpg' #name of the file to be saved
-            # print('Creating...' + name) #Creating frame by frame
-            # cv2.imwrite(name, frame) #saving the frame to the folder images
-            currentframe += 1 #incrementing the frame number
-        else:
-            break
-    cam.release()
-    cv2.destroyAllWindows()
+import cv2
+import os
+
+face_cascade = cv2.CascadeClassifier('Masked-Face-Recognition-from-CCTV-Footage/Local/frameExtractor.py')
+
+def detect_faces(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
+
+video_path = 'video.mp4'
+output_dir = 'outputs/'
+
+cap = cv2.VideoCapture(video_path)
+face_count = 0
+face_files = set()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    faces = detect_faces(frame)
+    for (x, y, w, h) in faces:
+        face_img = frame[y:y+h, x:x+w]
+        face_filename = f'face_{face_count}.jpg'
+        face_path = os.path.join(output_dir, face_filename)
+        
+        # Store the face image
+        cv2.imwrite(face_path, face_img)
+        
+        # Keep track of unique face files
+        face_files.add(face_filename)
+        
+        face_count += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+print(f"Found {len(face_files)} distinct faces.")
